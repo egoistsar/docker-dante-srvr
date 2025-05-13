dc9566ee312fe407dd1afb46658d5fb7ebee608a
egoistsar <egoistsar@gmail.com>
2025-05-14 00:09:21 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -109,9 +109,6 @@ echo "🐳 Собираю Docker-образ..."
 DOCKER_BUILD_LOG=$(mktemp)
 docker build --no-cache -t dante-proxy-auto . >"$DOCKER_BUILD_LOG" 2>&1 || BUILD_FAILED=1
 
-# if grep -q "apparmor_parser" "$DOCKER_BUILD_LOG"; then
- # echo "⚠️ AppArmor включён, но apparmor_parser не найден. Проблема проигнорирована."
-# fi
 
 if [[ "$BUILD_FAILED" == "1" ]] && ! docker image inspect dante-proxy-auto >/dev/null 2>&1; then
   echo "❌ Ошибка при сборке Docker-образа:"
